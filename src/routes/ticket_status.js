a2a5a7429dd94a450f018217fa309d4f611f6625
Gian Edrisandi <giansandi11@gmail.com>
2022-12-25 21:27:37 +0700

    update ticket_status

--- src/routes/ticket_status.js
+++ src/routes/ticket_status.js
@@ -4,6 +4,6 @@ const { ticketStatusController } = require("../controller/ticket_status");
 
 router.get("/", ticketStatusController.getTicketStatus);
 router.post("/", ticketStatusController.insert);
-router.put("/put-ticket-status/:id", ticketStatusController.update);
-router.delete("/delete-ticket-status/:id",ticketStatusController.delete);
+router.put("//:id", ticketStatusController.update);
+router.delete("/:id",ticketStatusController.delete);
 module.exports = router;

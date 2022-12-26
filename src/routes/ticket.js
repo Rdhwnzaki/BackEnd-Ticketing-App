8d403ec786ea215c022f36ed474224826c974d31
Fanani23 <pramudiasf23@gmail.com>
2022-12-26 11:09:55 +0700

    solve get by search

--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { ticketController } = require("../controller/ticket")
+const { ticketController } = require("../controller/ticket");
 
-router.post("/register",ticketController.insert)
-router.get("/getstockticket",ticketController.getTicket)
-router.delete("/delete/:id",ticketController.delTicket)
-router.put("/edit/:id",ticketController.editTicket)
-router.get("/pagination",ticketController.getTicketsearch)
+router.post("/register", ticketController.insert);
+router.get("/getstockticket", ticketController.getTicket);
+router.get("/pagination", ticketController.getTicketsearch);
+router.delete("/delete/:id", ticketController.delTicket);
+router.put("/edit/:id", ticketController.editTicket);
 
 module.exports = router;
